a140b0fd7c55c74b943ee97ee447b91d3a1782be
Gabriel Santos <gabmaxsan@gmail.com>
2019-03-26 23:24:04 -0300

    start gulp module

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+
+gulp.task('default', () => {
+    gulp.start('copiar','fim')
+})
+
+gulp.task('copiar', ['antes1','antes2'], () => {
+    gulp.src(['pastaA/arquivo1.txt','pastaA/arquivo2.txt'])
+        .pipe(gulp.dest('pastaB'))
+})
+
+gulp.task('antes1', () => {
+    console.log('Antes1!!')
+})
+
+gulp.task('antes2', () => {
+    console.log('Antes2!!!')
+})
+
+gulp.task('fim', () => {
+    console.log('Fim!!!')
+})
